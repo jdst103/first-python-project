d60403e64c036e51b283855f2fbb1275552b36ca
Jason Robinson <jrobinson@spartaglobal.com>
2020-02-04 21:33:00 +0000

    created print conversation summary on user characteristics

--- first exercise.py
+++ first exercise.py
@@ -1,27 +1,26 @@
 # # creating a chatbox basically
-
 # Define the following variables
 # name, last_name, species, eye_color, hair_color
-# name = 'Lana'
+
 name = 'Steph'
 last_name = 'Robinson'
 species = 'giraffe'
 eye_colour = 'Green'
 hair_colour = 'Blonde'
+age = 23
 
 # Prompt user for input and Re-assign these
-# name = input('what new name would you like?')
 name = input('What new name would you like to have?')
 last_name = input('what new last name would you like to have?')
-species = input('what new specie would you like to be?')
+species = input('what new species would you like to be?')
 eye_colour = input('what new eye colour do you want?')
 hair_colour = input('what new hair colour would you like?')
-
-
+age = input('how old would you like to be?')
 
 # Print them back to the user as conversation
-# Example: 'Hello Jack! Welcome, your age is 26, your eyes are green and your hair color is black.
+print(f'Hello {name}, you are now a {species}, your age is {age}, your eyes are {eye_colour} and your hair colour is {hair_colour}!')
 
 # Section 2 - Calculate in what year was the person born? and responde back.
-# print something like: 'You said you we're 28 hence you were born in 1991!'
+
+
 
