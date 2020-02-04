6f37bccb7667f8632ef80005539b7c21c0cfc782
Jason Robinson <jrobinson@spartaglobal.com>
2020-02-04 21:09:28 +0000

    added user inputs

--- first exercise.py
+++ first exercise.py
@@ -11,6 +11,11 @@ hair_colour = 'Blonde'
 
 # Prompt user for input and Re-assign these
 # name = input('what new name would you like?')
+name = input('What new name would you like to have?')
+last_name = input('what new last name would you like to have?')
+species = input('what new specie would you like to be?')
+eye_colour = input('what new eye colour do you want?')
+hair_colour = input('what new hair colour would you like?')
 
 
 
