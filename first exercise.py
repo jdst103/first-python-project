9efc7f659a7e4299b7064cd726919a19f46fed59
Jason Robinson <jrobinson@spartaglobal.com>
2020-02-04 21:02:18 +0000

    defined my variables

---
+++ first exercise.py
@@ -0,0 +1,22 @@
+# # creating a chatbox basically
+
+# Define the following variables
+# name, last_name, species, eye_color, hair_color
+# name = 'Lana'
+name = 'Steph'
+last_name = 'Robinson'
+species = 'giraffe'
+eye_colour = 'Green'
+hair_colour = 'Blonde'
+
+# Prompt user for input and Re-assign these
+# name = input('what new name would you like?')
+
+
+
+# Print them back to the user as conversation
+# Example: 'Hello Jack! Welcome, your age is 26, your eyes are green and your hair color is black.
+
+# Section 2 - Calculate in what year was the person born? and responde back.
+# print something like: 'You said you we're 28 hence you were born in 1991!'
+
