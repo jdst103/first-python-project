19e03125355812fb8671f0a2cff70c34d5cb8fce
Jason Robinson <jrobinson@spartaglobal.com>
2020-02-05 10:54:12 +0000

    added strip to my code

--- first exercise.py
+++ first exercise.py
@@ -10,18 +10,31 @@ hair_colour = 'Blonde'
 age = 23
 
 # Prompt user for input and Re-assign these
-name = input('What new name would you like to have?')
-last_name = input('what new last name would you like to have?')
-species = input('what new species would you like to be?')
-eye_colour = input('what new eye colour do you want?')
-hair_colour = input('what new hair colour would you like?')
-age = input('how old would you like to be?')
+#also capitising names and stripping any white space
+name = input('What new name would you like to have?     :')
+name = name.strip().capitalize()
+
+last_name = input('what new last name would you like to have?       :')
+last_name = last_name.strip().capitalize()
+
+species = input('what new species would you like to be?     :')
+species = species.strip()
+
+eye_colour = input('what new eye colour do you want?        :')
+eye_colour = eye_colour.strip()
+
+hair_colour = input('what new hair colour would you like?       :')
+hair_colour = hair_colour.strip()
+
+age = input('how old would you like to be?     :')
+age = age.strip()
 
 # Print them back to the user as conversation
-print(f'Hello {name}, you are now a {species}, your age is {age}, your eyes are {eye_colour} and your hair colour is {hair_colour}!')
+print(f'Hello {name} {last_name}, you are now a {species}, your age is {age},  your eyes are {eye_colour} and your hair colour is {hair_colour}!')
 
 # Section 2 - Calculate in what year was the person born? and responde back.
-new_age = input('how old are you again?')
+new_age = input('how old are you again? (please use numerical values in years)')
+new_age = new_age.strip()
 birth_year = 2020 - int(new_age)
 
 print(f'since you said you was {new_age}, you was born in {birth_year}!')
